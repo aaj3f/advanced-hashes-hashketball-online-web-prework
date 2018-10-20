175b323a5b13011e42d2d98eecf3195c7180c1ed
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-20 13:57:58 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -134,7 +134,7 @@ end
 def num_points_scored(player_name)
   game_hash.each do |location, team_data|
     team_data.each do |attribute, data|
-      if data.keys.include?(player_name)
+      if data.include?(player_name)
         data[player_name][:points]
       end
     end
