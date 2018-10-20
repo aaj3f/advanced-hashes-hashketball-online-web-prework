54120ad82ade96581b419e2f8723490cd1c53024
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-20 14:09:13 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -159,7 +159,7 @@ def team_colors(team_name)
   colors = []
   game_hash.each do |location, team_data|
     if team_data[:team_name] == team_name
-      colors << [:colors]
+      colors << team_data[:colors]
     end
   end
   colors
